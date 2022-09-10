f5d634c2cf97af3312d010ccc3b468cc08adc058
godson-omagbemi <godsongbemi@gmail.com>
2022-09-10 05:19:31 -0700

    variables

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,16 +7,15 @@
  */
 int main(void)
 {
-int ch;
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'q' && ch != 'e')
+{
+putchar(ch);
+}
+}
 
-for (ch = 97; ch <= 122; ch++)
-{
-putchar(ch);
-}
-for (ch = 65; ch <= 90; ch++)
-{
-putchar(ch);
-}
 putchar(10); /* this is an ascii code for new line*/
 
 return (0);
