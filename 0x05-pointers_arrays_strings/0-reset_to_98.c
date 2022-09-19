5b8f9a8f6d79871ee03931499b8861cd04435d2d
godson-omagbemi <godsongbemi@gmail.com>
2022-09-19 08:23:26 -0700

    array strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,13 @@
-include "main.h"
+#include "holberton.h"
 /**
- * reset_to_98 - takes a pointer to a parameter and updates its value.
- * @n: input integer.
- * Return: no return.
- */
+ * reset_to_98 - reset argument to 98
+ * @n: pointer
+ * Return: void
+*/
+
 void reset_to_98(int *n)
 {
+
 *n = 98;
+
 }
